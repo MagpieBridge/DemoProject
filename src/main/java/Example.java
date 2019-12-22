8bb9f6474d6bc469be6cfdf6158d1c080c451526
Linghui Luo <linghui@mail.upb.de>
2019-12-22 21:47:24 +0100

    add code for tutorial2

---
+++ src/main/java/Example.java
@@ -0,0 +1,8 @@
+  public class Example()
+  {
+    public static void main(String... args) {
+       String data = "some data";
+       String key = "secret key";
+       Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    }
+  }
